10530dfa570abb08b410a9e398173f1b7c5bdd32
SanZangana <San9@live.se>
2022-10-18 15:30:25 +0000

    Separate questions into own file

---
+++ assets/js/questions.js
@@ -0,0 +1,40 @@
+/* jshint esversion: 11 */
+
+const questions = [
+    {
+        question: 'What is 2 + 2',
+        answers: [
+            {text: '4', correct: true},
+            {text: '22', correct: false},
+            {text: '55', correct: false},
+            {text: '89', correct: false},
+        ]
+    },
+    {
+        question: 'What is 4 + 4',
+        answers: [
+            {text: '8', correct: true},
+            {text: '22', correct: false},
+            {text: '48', correct: false},
+            {text: '9', correct: false},
+        ]
+    },
+    {
+        question: 'What is 10 + 5',
+        answers: [
+            {text: '15', correct: true},
+            {text: '10', correct: false},
+            {text: '35', correct: false},
+            {text: '20', correct: false},
+        ]
+    },
+    {
+        question: 'What is 15 + 15',
+        answers: [
+            {text: '30', correct: true},
+            {text: '45', correct: false},
+            {text: '40', correct: false},
+            {text: '50', correct: false},
+        ]
+    }
+];
