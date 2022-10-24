06c2a3cf27b028fa10c443a985265967cebdc306
SanZangana <San9@live.se>
2022-10-24 07:01:36 +0000

    Add more questions and answers to quiz

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,39 +2,102 @@
 
 const questions = [
     {
-        question: 'What is 2 + 2',
+        question: 'Whats the main characters name in Berserk?',
         answers: [
-            {text: '4', correct: true},
-            {text: '22', correct: false},
-            {text: '55', correct: false},
-            {text: '89', correct: false},
+            {text: 'Guts', correct: true},
+            {text: 'Naruto', correct: false},
+            {text: 'Luffy', correct: false},
+            {text: 'Akira', correct: false},
         ]
     },
     {
-        question: 'What is 4 + 4',
+        question: 'What is Narutos beast called?',
         answers: [
-            {text: '8', correct: true},
-            {text: '22', correct: false},
-            {text: '48', correct: false},
-            {text: '9', correct: false},
+            {text: 'Nine-tails', correct: true},
+            {text: 'Ox-tails', correct: false},
+            {text: 'Sharingan', correct: false},
+            {text: 'Chakra', correct: false},
         ]
     },
     {
-        question: 'What is 10 + 5',
+        question: 'What was Griffiths dream?',
         answers: [
-            {text: '15', correct: true},
-            {text: '10', correct: false},
-            {text: '35', correct: false},
-            {text: '20', correct: false},
+            {text: 'a Kingdom', correct: true},
+            {text: 'an Army', correct: false},
+            {text: 'Money', correct: false},
+            {text: 'Fame', correct: false},
         ]
     },
     {
-        question: 'What is 15 + 15',
+        question: 'What is Madaras plan called?',
         answers: [
-            {text: '30', correct: true},
-            {text: '45', correct: false},
-            {text: '40', correct: false},
-            {text: '50', correct: false},
+            {text: 'Infinite Tsukyomi', correct: true},
+            {text: 'Operation Red', correct: false},
+            {text: 'Dream over', correct: false},
+            {text: 'Fantasy land', correct: false},
         ]
-    }
+    },
+    {
+        question: 'What is Thorfinns philosophy?',
+        answers: [
+            {text: 'Nobody has enemies', correct: true},
+            {text: 'Violence solves everything', correct: false},
+            {text: 'Conquer the lands', correct: false},
+            {text: 'Always avenge your fallen comrades', correct: false},
+        ]
+    },
+    {
+        question: 'What is Denjis dog called?',
+        answers: [
+            {text: 'Pochita', correct: true},
+            {text: 'Chiquita', correct: false},
+            {text: 'Rosalita', correct: false},
+            {text: 'Frank', correct: false},
+        ]
+    },
+    {
+        question: 'How can Kaneki survive?',
+        answers: [
+            {text: 'By eating humans', correct: true},
+            {text: 'By fasting', correct: false},
+            {text: 'By consuming milk', correct: false},
+            {text: 'By eating sugar', correct: false},
+        ]
+    },
+    {
+        question: 'What is Erens brother called?',
+        answers: [
+            {text: 'Zeke Jaeger', correct: true},
+            {text: 'Donald Dagger', correct: false},
+            {text: 'Mick Jagger', correct: false},
+            {text: 'Dwayne Stone', correct: false},
+        ]
+    },
+    {
+        question: 'Whats the egg of the king called in Berserk?',
+        answers: [
+            {text: 'Behelit', correct: true},
+            {text: 'Rinnegan', correct: false},
+            {text: 'Jinchuriki', correct: false},
+            {text: 'Yubi', correct: false},
+        ]
+    },
+    {
+        question: 'What is Lights shinigami called?',
+        answers: [
+            {text: 'Ryuk', correct: true},
+            {text: 'Danzo', correct: false},
+            {text: 'Casca', correct: false},
+            {text: 'Askeladd', correct: false},
+        ]
+    },
+    {
+        question: 'Which titan is the biggest?',
+        answers: [
+            {text: 'The Founding Titan', correct: true},
+            {text: 'Attack Titan', correct: false},
+            {text: 'Colossal Titan', correct: false},
+            {text: 'Armoured Titan', correct: false},
+        ]
+    },
 ];
